1d43700b861bf6245044c6893fbf11791ceb33e9
Ceylin Yasar <ceylinrukiyeyasar@gmail.com>
2023-02-28 20:08:01 +0300

    ödev tamamlandı

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,23 @@
-// ESNEK
+const defaultCars = [
+  {
+    vin: "34544444444444444",
+    make: "toyota",
+    model: "hilux",
+    mileage: 15000,
+    title: "clean",
+    transmission: "automatic",
+  },
+  {
+    vin: "24567899999999999",
+    make: "hyundai",
+    model: "i20",
+    mileage: 60000,
+    title: "clean",
+    transmission: "automatic",
+  },
+];
+
+exports.seed = async function (knex) {
+  await knex("cars").truncate();
+  await knex("cars").insert(defaultCars);
+};
